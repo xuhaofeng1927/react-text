a03990419f04e2209ee81026d6641a43a049a22c
xuhaofeng1927 <441345737@qq.com>
2020-02-29 16:39:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-j8bcqt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-j8bcqt)
---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+var h1 = (
+  <div>
+    <ul>
+    <li>123445566777</li>
+    <li>123445566777</li>
+    <li>123445566777</li>
+    <li>123445566777</li>
+    </ul>
+  </div>
+)
+render(h1, document.getElementById('root'));

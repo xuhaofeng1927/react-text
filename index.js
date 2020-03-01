069dfa5342144580113f03c3e7c8a15a93e616bf
xuhaofeng <xuhaofeng@163.com>
2020-03-01 09:04:12 +0800

    img

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ var h1 = (
     <li>这是个非常好用的云开发</li>
     <li>这是个非常好用的云开发</li>
     </ul>
+    <h1>这是我的女朋友</h1>
+    <div className = "box">
+      <img src="./img/9b7010d75cf6df3573c291d7bd2e337e5bd5b82a.jpg" alt="">
+    </div>
+    <div className = "box">
+      <img src="./img/1578933508726.jpg" alt="">
+    </div>
+    <div className = "box">
+      <img src="" alt="./img/1578933508726.png">
+    </div>
   </div>
 )
 render(h1, document.getElementById('root'));

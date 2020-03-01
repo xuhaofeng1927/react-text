57edf537f67f71e5edbda7f56a23c99c9ca23f1c
xuhaofeng1927 <441345737@qq.com>
2020-03-01 09:12:40 +0800

    1 file modified

--- index.js
+++ index.js
@@ -2,41 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
-
 var h1 = (
   <div>
-    <ul>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    <li>这是个非常好用的云开发</li>
-    </ul>
     <h1>这是我的女朋友</h1>
     <div className = "box">
-      <img src="./img/9b7010d75cf6df3573c291d7bd2e337e5bd5b82a.jpg" alt="">
+      <img src="./img/9b7010d75cf6df3573c291d7bd2e337e5bd5b82a.jpg" alt=""/>
     </div>
     <div className = "box">
-      <img src="./img/1578933508726.jpg" alt="">
+      <img src="./img/1578933508726.jpg" alt=""/>
     </div>
     <div className = "box">
-      <img src="" alt="./img/1578933508726.png">
+      <img src="" alt="./img/1578933508726.png"/>
     </div>
   </div>
 )
